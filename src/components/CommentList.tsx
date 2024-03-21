59710ff91aca92c3f2dd950850940324dcb90b49
y6310 <hey1hey2hey3go@gmail.com>
2024-03-21 16:28:40 +0900

    fix:css追加

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Posts } from '../models';
 
 interface commentListProps {
-    comments: Posts[]; // nitamoyasの型を指定
+    comments: Posts[]; 
 
   }
 
 const CommentList: React.FC<commentListProps> = ({comments}) => {
+  
   return (
     <div>
         <ul>
             {comments.slice().reverse().map(comment =>(//新しい投稿を上に表示させるために逆順
-                <div key = {comment.post_id}>
+                <div key = {comment.post_id} className="bg-white pt-10 pb-10 pl-10 pr-10 mb-10 items-center justify-center rounded-lg">
                   <p>{comment.user_name}</p>
                     <p>{comment.post_body}</p>
                     {comment.tag && comment.tag.map(tag => (
